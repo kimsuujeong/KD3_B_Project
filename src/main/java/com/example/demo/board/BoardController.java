e68ddd5a857896efd3841483b57e1ee890b04558
Shinilwoo <sinsin4197@daum.net>
2024-03-19 10:08:58 +0900

    BoardController생성 및 작성, main,board,board_detail html 생성 및 작성

---
+++ src/main/java/com/example/demo/board/BoardController.java
@@ -0,0 +1,51 @@
+package com.example.demo.board;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.user.User;
+import com.example.demo.user.UserService;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	BoardService boardService;
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/")
+	public String main(Model model) {
+		List<Board> posts = boardService.getAllPosts();
+        model.addAttribute("posts", posts);
+		return "main";
+	}
+	@GetMapping("/board")
+    public String showBoard(@RequestParam(name = "categoryId", required = false) Integer categoryId, Model model) {
+		List<Board> posts = boardService.getAllPosts(); 
+	    for (Board post : posts) {
+	    	String userID = post.getUser_userID().getUserID();
+	    	User user = userService.getUserById(post.getUser_userID());
+	        post.setUser_userID(user); 
+	    }
+
+        model.addAttribute("posts", posts);
+        return "board";
+    }
+	
+	@GetMapping("/board/{postID}")
+    public String showPostDetail(@PathVariable(name="postID") Integer postID, Model model) {
+        Board post = (Board) boardService.getPostById(postID);
+        
+//        System.out.println("컨트롤러" + post.getCategoryName());
+        model.addAttribute("post", post);
+        return "board_detail";
+    }
+}
