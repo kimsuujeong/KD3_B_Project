0eefe90b9119b1bd7347840351f30dfc225f4ab9
leegaeun <gaeun120312@gmail.com>
2024-03-20 09:48:22 +0900

    Update

---
+++ src/main/java/com/example/demo/Board.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="board")
+public class Board {
+
+	
+	
+}
