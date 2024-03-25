e33ae3b9afba43aed866c27e615cfb3471d45752
ad-seoil <ad.seoil0112@gmail.com>
2024-03-25 09:47:28 +0900

    예시 이미지 추가 및 드래그어블 스크롤 구현 carousel 일부구현

--- src/main/resources/static/mainpage/Main.js
+++ src/main/resources/static/mainpage/Main.js
@@ -1,49 +1,67 @@
-// img slider
-let list = document.querySelector('.slider .list');
-let items = document.querySelector('.slider .list .item');
-let dots = document.querySelector('.slider .dots li')
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
+// banner img slider
 
-let active = 0;
-let lengthItems = items.length - 1;
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+let btn3 = document.querySelector('.btn3');
 
-next.onclick = function(){
-    if(active + 1 > lengthItems){
-        active = 0;
-    }
-    else{
-        active = active + 1;
-    }
-    reloadSlider();
+btn1.addEventListener('click', function(){
+    document.querySelector('.banner-img-slider-container').style.transform = 'translate(0vw)';
+});
+
+btn2.addEventListener('click', function(){
+    document.querySelector('.banner-img-slider-container').style.transform = 'translate(-100vw)';
+});
+
+btn3.addEventListener('click', function(){
+    document.querySelector('.banner-img-slider-container').style.transform = 'translate(-200vw)';
+});
+
+// slider = banner-img-slider
+// list = banner-img-slider-container
+// item = inner
+
+// let list = document.querySelector('.banner-img-slider-container');
+// let items = document.querySelectorAll('.inner');
+// let dots = document.querySelectorAll('.dots li');
+// let prev = document.getElementById('prev');
+// let next = document.getElementById('next');
+
+// let active = 0;
+
+// console.log(list);
+
+// next.addEventListener('click', function(){
+//     active += 1;
+// });
+
+
+
+
+// scroll content
+const carousel = document.querySelector('.carousel');
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+let dragStart = (e) => {
+    // updating global variables valueon mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
 }
 
-prev.onclick = function(){
-    if(active - 1 < 0){
-        active = lengthItems;
-    }
-    else{
-        active = active - 1;
-    }
-    reloadSlider();
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
-let refreshSlider = setInterval(() => {next.click()}, 5000);
-
-function reloadSlider(){
-    let checkLeft = items[active].offsetLeft;
-    list.style.left = -checkLeft + 'rem';
-
-    let lastActiveDot = querySelector('.slider .dots li.active');
-    lastActiveDot.classList.remove('active');
-    dots[active].classList.add('active');
-    clearInterval(refreshSlider);
-    refreshSlider = setInterval(() => {next.click()}, 5000);
+const dragStop = () => {
+    isDragStart = false;
 }
 
-dots.forEach((li, key) =>{
-    li.addEventListner('click', function(){
-        active = key;
-        reloadSlider();
-    })
-})
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+// scroll content END
