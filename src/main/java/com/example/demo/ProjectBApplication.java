0e950af68ba7d5f8da284868e78239ab46516824
ad-seoil <159258408+ad-seoil@users.noreply.github.com>
2024-03-27 10:01:23 +0900

    test

--- src/main/java/com/example/demo/ProjectBApplication.java
+++ src/main/java/com/example/demo/ProjectBApplication.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
-
 // @SpringBootApplication
 // Security 로그인 페이지 비활성화 처리 해줬습니다.
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
