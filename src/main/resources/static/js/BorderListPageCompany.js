7230a9a5e8cabb38a3d2d8eb431592fd9341b0d0
ad.seoil <ad.seoil0112@gmail.com>
2024-04-01 16:50:41 +0900

    어긋난 static파일 재정리 및 BoarderListPage 회사용, 예술인용 작업 완료

--- src/main/resources/static/js/BorderListPageCompany.js
+++ src/main/resources/static/js/BorderListPageCompany.js
@@ -8,3 +8,36 @@ setInterval(function(){
     }
 }, 5000);
 // img slider END
+
+// pagination
+let link = document.querySelectorAll('.link');
+let currentValue = 1;
+
+function activeLink(){
+    for(let l of link){
+        l.classList.remove("active");
+    }
+    event.target.classList.add("active");
+    currentValue = event.target.value;
+};
+
+function backBtn(){
+    if(currentValue > 1){
+        for(let l of link){
+            l.classList.remove("active");
+        }
+        currentValue--;
+        link[currentValue - 1].classList.add("active");
+    }
+};
+
+function nextBtn(){
+    if(currentValue < 6){
+        for(let l of link){
+            l.classList.remove("active");
+        }
+        currentValue++;
+        link[currentValue - 1].classList.add("active");
+    }
+}
+// after 5 is Error
