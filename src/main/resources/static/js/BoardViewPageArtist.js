c66901e01a6e782e8554a3313891c6aa49db7dc7
ad.seoil <ad.seoil0112@gmail.com>
2024-04-02 16:45:49 +0900

    인터넷 이슈로 인해 업로드 되지 않았던 파일 재 업로드

---
+++ src/main/resources/static/js/BoardViewPageArtist.js
@@ -0,0 +1,33 @@
+// scroll content
+const carousel = document.querySelectorAll('.carousel');
+
+for(let target of carousel){
+
+    let isDragStart = false, prevPageX, prevScrollLeft;
+
+    let dragStart = (e) => {
+        // updating global variables valueon mouse down event
+        isDragStart = true;
+        prevPageX = e.pageX;
+        prevScrollLeft = target.scrollLeft;
+    }
+
+    const dragging = (e) => {
+        // scrolling images/carousel to left according to mouse pointer
+        if(!isDragStart) return;
+        e.preventDefault();
+        let positionDiff = e.pageX - prevPageX;
+        target.scrollLeft = prevScrollLeft - positionDiff;
+    }
+
+    const dragStop = () => {
+        isDragStart = false;
+    }
+
+
+    target.addEventListener("mousedown", dragStart);
+    target.addEventListener("mousemove", dragging);
+    target.addEventListener("mouseup", dragStop);
+}
+
+// scroll content END
