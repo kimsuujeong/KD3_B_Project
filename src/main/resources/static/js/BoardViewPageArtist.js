d21a01494651f6f3750620ab5e183222c08a5d09
ad.seoil <ad.seoil0112@gmail.com>
2024-04-03 14:01:32 +0900

    상세 페이지 예술인용 작업중

--- src/main/resources/static/js/BoardViewPageArtist.js
+++ src/main/resources/static/js/BoardViewPageArtist.js
@@ -31,3 +31,45 @@ for(let target of carousel){
 }
 
 // scroll content END
+
+
+// trailer img
+const slider = document.querySelector('.trailer-img-slider');
+const trailerCarousel = document.querySelector('.trailer-img-carousel');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+
+var direction;
+
+prev.addEventListener('click', function(){
+    if(direction === -1){
+        direction = 1;
+        slider.appendChild(slider.firstElementChild);
+    }
+    trailerCarousel.style.justifyContent = 'flex-end'; 
+    slider.style.transform = 'translate(20%)';
+});
+
+next.addEventListener('click', function(){
+    direction = -1;
+    trailerCarousel.style.justifyContent = 'flex-start'; 
+    slider.style.transform = 'translate(-20%)';
+});
+
+slider.addEventListener('transitioned', function(){
+    if(direction === -1){
+        slider.appendChild(slider.firstElementChild);
+    } else if(direction === 1){
+        slider.prepend(slider.lastElementChild);
+    }
+    
+
+    slider.style.transition = 'none';
+    slider.style.transform = 'translate(0)';
+    setTimeout(function(){
+        slider.style.transition = 'all 0.5s';
+    })
+    
+});
