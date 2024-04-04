6f4dbc1b3ba9d41e4d5b325e9a3bf6bce1659a51
leegaeun <gaeun120312@gmail.com>
2024-04-04 09:24:48 +0900

    관심 목록 기능 구현

--- src/main/java/com/example/demo/like/LikeController.java
+++ src/main/java/com/example/demo/like/LikeController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,10 +21,11 @@ public class LikeController {
 	@Autowired
 	LikeService likeService;
 	
+	// 관심 목록 리스트
 	@RequestMapping(value = "/list")
 	public ModelAndView list(HttpSession session, ModelAndView mv) {
 		
-		Map<String, Object> map = new HashMap<>();
+		Map<String, Object> map = new HashMap<String, Object>();
 		//String userID = (String) session.getAttribute("userID");
 		String userID = "test1";
 		
@@ -39,23 +42,36 @@ public class LikeController {
 		}
 	}
 	
+	// 관심 목록 추가
 	@RequestMapping(value = "/insert", method = {RequestMethod.POST})
 	public String insert(Like like, HttpSession session) {
 		
 		//String userID = (String) session.getAttribute("userID");
 		
+		// 임시로 userID 설정 (로그인 연결 후 수정)
 		String userID = "test1";
 //		if(userID == null) {
 //			return "redirect:/TestMain";
 //		}
 		
 		like.setUserID(userID);
-		likeService.insert(like);
 		
+		// 관심 목록에 있는지 검사
+		int count = likeService.countLike(like.getPostID(), userID);
+		if(count == 0) {
+			likeService.insert(like);
+		} else {
+			return "/TestHtml/board/like_popup";
+		}
+	
 		return "redirect:/like/list";
 	}
 	
-	
-	
+	// 관심 목록 삭제
+	@RequestMapping(value = "/delete/{likeID}")
+	public String delete(@PathVariable(name = "likeID") Integer likeID) {
+		likeService.delete(likeID);
+		return "redirect:/like/list";
+	}
 	
 }
