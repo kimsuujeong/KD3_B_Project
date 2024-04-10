3ad3b26eb480d91762d303fa6ae3dc5d55979c1d
kimsuujeong <thehapy0103@naver.com>
2024-04-10 17:55:55 +0900

    회원가입 (js,ajax,예외처리)구현 완료

--- src/main/java/com/example/demo/userJoin/UserJoinController.java
+++ src/main/java/com/example/demo/userJoin/UserJoinController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,7 +21,7 @@ import com.example.demo.user.User;
 public class UserJoinController {
 
 	@Autowired
-	UserJoinService userService;
+	UserJoinService userJoinService;
 
 	@Autowired
 	private RedisUtil redisUtil;
@@ -30,8 +29,8 @@ public class UserJoinController {
 	@Autowired
 	private MailController mailController;
 
-	private String TokenEmail = null;
-	private String Email = null;
+	private String TokenEmail = null; // 이메일 요청 처리 유,무
+	private String Email = null; // 이메일 토큰 처리 유,무
 
 	@GetMapping("/joinForm")
 	public String joinForm() {
@@ -40,27 +39,30 @@ public class UserJoinController {
 	// 이메일 요청 처리
 	@ResponseBody
 	@PostMapping("/submitEmailToken")
-	public ResponseEntity<Map<String, String>> joinForm(@RequestParam("email") String email,
+	public ResponseEntity<Map<String, String>> postEmail(@RequestParam("email") String email,
 			@ModelAttribute User userDTO, Model model) {
 	    Map<String, String> response = new HashMap<>();
 
 	    try {
-	        if (userService.emailchk(email) != 1) {
+	        if (userJoinService.emailchk(email) != 1) {
 	        	
-	        	mailController.mailSend(email); // 이메일 보내기
+//	        	mailController.mailSend(email); // 이메일 보내기
 	        	
 	            response.put("redirectUrl", "http://localhost:8085/joinForm");
 	            response.put("message", "토큰이 보내졌습니다.");
-	            System.out.println(email);
 	            
 	            TokenEmail = email;
 	            
 	            return ResponseEntity.ok().body(response);
+	            
 	        } else {
+	        	
 	            response.put("redirectUrl", "http://localhost:8085/joinForm");
 	            response.put("message", "이미 가입된 이메일입니다.");
 	            return ResponseEntity.ok().body(response);
+	            
 	        }
+	        
 	    } catch (Exception e) {
 	        // 예외 처리
 	    }
@@ -87,7 +89,9 @@ public class UserJoinController {
 	            response.put("redirectUrl", "http://localhost:8085/joinForm");
 	            response.put("message", "토큰 인증되었습니다.");
 	            
-	            TokenEmail = Email;
+	            this.Email = TokenEmail;
+	            
+	            System.out.println("Email : " + Email);
 	            
 	            return ResponseEntity.ok().body(response);
 	            
@@ -102,14 +106,162 @@ public class UserJoinController {
 	    }
 	    return ResponseEntity.badRequest().body(response);
 	}
+	
 	// 아이디 중복 체크 처리
 	@PostMapping("/idCheck")
 	@ResponseBody
-	public int idCheck(@RequestParam("id") String id) {
+	public int idCheck(@RequestParam("id") String id, @RequestParam("type") String type){
 		
-		int cnt = userService.idchk(id);
+		int cnt = userJoinService.idchk(id);
 		
-		return cnt;
+		if (cnt==1) {
+			System.out.println(id); // test
+			return 1;
+		}
+		
+		System.out.println(id); //test
+		return 0;
 		
 	}
+	
+	// 아이디 중복 체크 처리
+	@PostMapping("/nicknameCheck")
+	@ResponseBody
+	public int nicknameCheck(@RequestParam("nickname") String nickname, @RequestParam("type") String type){
+		
+		int cnt = userJoinService.namechk(nickname);
+		
+		if (cnt==1) {
+			System.out.println(nickname); // test
+			return 1;
+		}
+		
+		System.out.println(nickname); //test
+		return 0;
+		
+	}
+	
+	@PostMapping("/joinForm")
+	@ResponseBody
+	public ResponseEntity<Map<String, String>> joinForm(@RequestParam("email") String email,
+														@RequestParam("email_Check_key") String email_Check_key,
+														@RequestParam("mid_id") String mid_id,
+														@RequestParam("mid_nickname") String mid_nickname,
+														@RequestParam("password") String password,
+														@RequestParam("password_ck") String password_ck,
+														@ModelAttribute User userDTO, Model model){
+		
+		String getKey = redisUtil.getData(Integer.parseInt(email_Check_key));
+		
+		Map<String, String> response = new HashMap<>();
+		
+		if (TokenEmail == null) {
+			System.out.println("이메일 인증 해주세요"); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "이메일 인증 해주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		if (Email == null) {
+			System.out.println("인증번호 확인 해주세요"); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "인증번호 확인 해주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		if (TokenEmail==Email) {
+			System.out.println("토큰 번호를 보낸 이메일과 다릅니다."); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "토큰 번호를 보낸 이메일과 다릅니다.");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		if (Email != null && Email != email) {
+			System.out.println("토큰 번호를 확인해 주세요"); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "토큰 번호를 확인해 주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		
+		if (email_Check_key == null) {
+			System.out.println("인증번호를 확인해 주세요"); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "인증번호를 확인해 주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		if (!Email.equals(getKey)) {
+			System.out.println("토큰번호를 다시 확인해 주세요");// test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "토큰번호를 다시 확인해 주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		if (userJoinService.idchk(mid_id)==1) {
+			System.out.println("아이디를 다시 확인해 주세요"); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "아이디를 다시 확인해 주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		if (userJoinService.namechk(mid_nickname)==1) {
+			System.out.println("닉네임을 다시 확인해 주세요"); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "닉네임을 다시 확인해 주세요");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		
+		if (!password.equals(password_ck)) {
+			System.out.println("비밀번호가 맞지 않습니다."); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "비밀번호가 맞지 않습니다.");
+
+			return ResponseEntity.ok().body(response);
+		}
+
+		try {
+			
+			if (Email.equals(email) && Email.equals(getKey) 
+					&& userJoinService.idchk(mid_id)==0
+					&& userJoinService.namechk(mid_nickname)==0
+					&& password.equals(password_ck)) {
+				
+				userDTO.setEmail(email);
+				userDTO.setUserID(mid_id);
+				userDTO.setUserName(mid_nickname);
+				userDTO.setPassword(password_ck);
+				
+				userJoinService.InsertUser(userDTO);
+				
+				System.out.println("회원가입 완료"); // test
+				
+				response.put("redirectUrl", "http://localhost:8085/Testmain");
+				response.put("message", "회원가입 완료");
+
+				return ResponseEntity.ok().body(response);
+				
+			}
+			
+		} catch (Exception e) {
+			System.out.println("서버 오류 입니다."); // test
+			response.put("redirectUrl", "http://localhost:8085/joinForm");
+			response.put("message", "서버 오류 입니다.");
+
+			return ResponseEntity.ok().body(response);
+		}
+		
+		return null;
+		
+	}
+
 }
--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,88 +1,296 @@
-/**
- *  회원가입 관련 처리
- */
+// 이메일 유효성 검사
+function sendVerificationCode() {
 
-function checkEmail() {
   var email = document.getElementById("email").value;
-  // 간단히 이메일 형식을 체크합니다.
-  if (!isValidEmail(email)) {
-    document.getElementById("emailMessage").innerHTML = "<strong style='color:red;'>유효한 이메일 주소를 입력하세요.</strong>";
-    return;
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!emailPattern.test(email)) {
+    alert("유효한 이메일 주소를 입력하세요.");
+    return false;
   }
 
-//   // 간단히 이메일이 "admin@example.com"일 때만 중복된 것으로 처리합니다.
-//   if (email === "admin@example.com") {
-//     document.getElementById("emailMessage").innerHTML = "<strong style='color:red;'>이미 사용 중인 이메일 주소입니다.</strong>";
-//   } else {
-//     document.getElementById("emailMessage").innerHTML = "<strong style='color:green;'>사용 가능한 이메일 주소입니다.</strong>";
-//   }
+  // AJAX 요청
+  $.ajax({
+
+    url: "/submitEmailToken",
+    type: "POST",
+    dataType: "json",
+    data: { email: email },
+
+    success: function (response) {
+
+      console.log(response);
+      console.log(email);
+
+      var redirectUrl = response.redirectUrl;
+      var message = response.message;
+
+      alert(message); // 서버에서 보낸 메시지를 알림으로 표시
+
+    },
+    error: function () {
+
+      alert("서버오류입니다.");
+
+    }
+
+  });
+
+  // 기본 동작 방지
+  return false;
+
 }
 
-// 이메일 형식을 체크하는 함수
-function isValidEmail(email) {
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+// 토큰 검사
+function checkVerificationCode() {
+
+  var email_Check_key = document.getElementById("email_Check_key").value;
+  var tokenPattern = /^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$/;
+
+  console.log(email_Check_key);
+
+  if (email_Check_key == "") {
+    alert("토큰값을 입력해주세요.");
+    return false;
+  }
+
+  if (!tokenPattern.test(email_Check_key)) {
+    alert("유효한 토큰을 입력하세요.");
+    return false;
+  }
+
+  // AJAX 요청
+  $.ajax({
+
+    url: "/checkEmailToken",
+    type: "POST",
+    dataType: "json",
+    data: { email_Check_key: email_Check_key },
+
+    success: function (response) {
+
+      console.log(response);
+      console.log(email_Check_key);
+
+      var redirectUrl = response.redirectUrl;
+      var message = response.message;
+
+      alert(message); // 서버에서 보낸 메시지를 알림으로 표시
+
+    },
+    error: function () {
+
+      alert("서버오류입니다.");
+
+    }
+
+  });
+
+  // 기본 동작 방지
+  return false;
+
 }
 
-// function checkUsername() {
-//   var username = document.getElementById("username").value;
-//   // 여기에서는 예시로 간단히 길이가 5 이상인 경우에만 유효한 것으로 가정합니다.
-//   if (username.length < 5) {
-//     document.getElementById("usernameMessage").innerHTML = "<strong style='color: red;'>아이디는 5자 이상이어야 합니다.</strong>";
-//     return;
-//   }
+// 아이디 유효성 검사
+var $mid_id = $("#mid_id");
+$mid_id.on("keyup", function () { // 키보드에서 손을 땠을 때 실행
+  var regExp = /^[a-z]+[a-z0-9]{5,15}$/g;
+  var $id = $("#id");
 
-//   // 간단히 아이디가 "admin"인 경우에만 중복된 것으로 처리합니다.
-//   if (username === "admin") {
-//     document.getElementById("usernameMessage").innerHTML = "<strong style='color: red;'>이미 사용 중인 아이디입니다.</strong>";
-//   } else {
-//     document.getElementById("usernameMessage").innerHTML = "<strong style='color: green;'>사용 가능한 아이디입니다.</strong>";
-//   }
-// }
+  if (!regExp.test($mid_id.val())) { // id 가 공백인 경우 체크
+    idchk = false;
+    $id.html("<span id='idcheck'>특수문자,공백 제외 영어로만 (5~15자 입력)</span>");
+    $("#idcheck").css({
+      "color": "#ff0000",
+      "font-weight": "bold",
+      "font-size": "10px"
+    })
+    return false;
+  } else { // 공백아니면 중복체크
+    $.ajax({
+      type: "POST", // http 방식 
+      url: "/idCheck", // ajax 통신 url
+      data: { // ajax 내용 => 파라미터 : 값 이라고 생각해도 무방
+        "type": "user",
+        "id": $mid_id.val()
+      },
+      success: function (data) {
 
-// function checkNickname() {
-//   var nickname = document.getElementById("nickname").value;
-//   // 여기에서는 예시로 간단히 길이가 3 이상인 경우에만 유효한 것으로 가정합니다.
-//   if (nickname.length < 3) {
-//     document.getElementById("nicknameMessage").innerHTML = "<strong style='color: red;'>닉네임은 3자 이상이어야 합니다.</strong>";
-//     return;
-//   }
+        if (data == 1) { // 1이면 중복
 
-//   // 간단히 닉네임이 "user"인 경우에만 중복된 것으로 처리합니다.
-//   if (nickname === "user") {
-//     document.getElementById("nicknameMessage").innerHTML = "<strong style='color: red;'>이미 사용 중인 닉네임입니다.</strong>";
-//   } else {
-//     document.getElementById("nicknameMessage").innerHTML = "<strong style='color: green;'>사용 가능한 닉네임입니다.</strong>";
-//   }
-// }
+          idchk = false;
+          $id.html("<span id='idcheck'>이미 존재하는 아이디입니다</span>")
+          $("#idcheck").css({
+            "color": "#ff0000",
+            "font-weight": "bold",
+            "font-size": "10px"
 
-// function submitForm() {
-//   window.location.href = "login.html";
-// }
+          })
+          return false;
 
-// 버튼 전체동의 (필수, 선택) 부분 코드
-document.addEventListener('DOMContentLoaded', () => {
-    const ckTag = document.getElementById('id_a');
-    const submitButton = document.querySelector('.submit');
-    const requiredCheckboxes = document.querySelectorAll('input[name="agree"]:required');
+        } else { // 아니면 중복아님
+          idchk = true;
+          $id.html("<span id='idcheck'>사용가능한 아이디입니다</span>")
 
-    ckTag.addEventListener('change', () => {
-        const allChecked = ckTag.checked;
+          $("#idcheck").css({
+            "color": "#9c9c9c",
+            "font-weight": "bold",
+            "font-size": "10px"
 
-        requiredCheckboxes.forEach(checkbox => {
-            checkbox.checked = allChecked;
-        });
+          })
+        }
+      }
+    })
+  }
+});
 
-        submitButton.disabled = !allChecked;
+// 닉네임 유효성 검사
+var $mid_nickname = $("#mid_nickname");
+var nicknamechk = false;
+$mid_nickname.on("keyup", function () {
+  var regExp = /^[a-z]+[a-z0-9]{5,15}$/g;
+  var $nickname = $("#nickname");
+
+  if ($mid_nickname.val() === "") {
+    nicknamechk = false; // 유효하지 않은 닉네임이 입력됐을 때 상태를 변경
+    $nickname.html("<span id='nickname'>닉네임을 입력해주세요.</span>").css({ // 스타일 적용하는 부분에 직접적으로 css 메소드를 체이닝
+      "color": "#ff0000",
+      "font-weight": "bold",
+      "font-size": "10px"
     });
+    return false;
+  } else {
+    $.ajax({
+      type: "POST",
+      url: "/nicknameCheck",
+      data: {
+        "type": "user",
+        "nickname": $mid_nickname.val()
+      },
+      success: function (data) {
+        if (data == 1) {
+          nicknamechk = false; // 중복된 닉네임이 있을 때 상태를 변경
+          $nickname.html("<span id='nickname'>이미 존재하는 아이디입니다</span>").css({ // 스타일 적용하는 부분에 직접적으로 css 메소드를 체이닝
+            "color": "#ff0000",
+            "font-weight": "bold",
+            "font-size": "10px"
+          });
+          return false;
+        }
+        else { // 아니면 중복아님
+          nicknamechk = true;
+          $nickname.html("<span id='nickname'>사용가능한 아이디입니다</span>")
+
+          $("#nickname").css({
+            "color": "#9c9c9c",
+            "font-weight": "bold",
+            "font-size": "10px"
+
+          })
+        }
+      }
+    });
+  }
+});
+
+
+// 비밀번호 유효성 검사
+var $password_ck = $("#password_ck");
+var $password = $("#password");
+$password_ck.on("keyup", function () {
+
+  if ($('#password').val() == $('#password_ck').val()) {
+    
+    $('#pwConfirm').text('비밀번호 일치').css({
+      "color": "#9c9c9c",
+      "font-weight": "bold",
+      "font-size": "10px"
+   
+    });
+
+  } else {
+
+    $('#pwConfirm').text('비밀번호 불일치').css({
+      "color": "#ff0000",
+      "font-weight": "bold",
+      "font-size": "10px"
+
+    });
+
+    return false;
+    
+  };;
+
+});
+
+// 데이터 insert 검사
+function validateForm() {
+
+  var email = document.getElementById("email").value;
+  var email_Check_key = document.getElementById("email_Check_key").value;
+  var mid_id = document.getElementById("mid_id").value;
+  var mid_nickname = document.getElementById("mid_nickname").value;
+  var password = document.getElementById("password").value;
+  var password_ck = document.getElementById("password_ck").value;
+
+
+  $.ajax({
+
+    url: "/joinForm",
+    type: "POST",
+    dataType: "json",
+    data: { email: email,
+            email_Check_key: email_Check_key,
+            mid_id: mid_id,
+            mid_nickname: mid_nickname,
+            password: password,
+            password_ck: password_ck
+          },
+
+    success: function (response) {
+
+      console.log(response);
+
+      var redirectUrl = response.redirectUrl;
+      var message = response.message;
+
+      alert(message); // 서버에서 보낸 메시지를 알림으로 표시
+      window.location.href = redirectUrl; // 리다이렉트
+
+    },
+    error: function () {
+
+      alert("서버 오류가 발생했습니다.");
+
+    }
+  });
+
+}
+
+// 유효성 검사
+document.addEventListener('DOMContentLoaded', () => {
+  const ckTag = document.getElementById('id_a');
+  const submitButton = document.querySelector('.submit');
+  const requiredCheckboxes = document.querySelectorAll('input[name="agree"]');
+
+  // 버튼 전체 동의 (필수, 선택) 부분 코드
+  ckTag.addEventListener('change', () => {
+    const allChecked = ckTag.checked;
 
     requiredCheckboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', () => {
-            const allAgreed = [...requiredCheckboxes].every(cb => cb.checked);
-            submitButton.disabled = !allAgreed;
+      checkbox.checked = allChecked;
+    });
 
-        });
+    submitButton.disabled = !allChecked;
+  });
+
+  requiredCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      const allAgreed = [...requiredCheckboxes].every(cb => cb.checked);
+      submitButton.disabled = !allAgreed;
 
     });
+
+  });
 
 });
